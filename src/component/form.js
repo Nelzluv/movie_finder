6064d6ec2d5dde2f64683abfc6093a1102051f65
Nelson Ejegi <nelzluv@gmail.com>
2020-05-09 14:43:44 +0100

    Update to searrchMovie function to use useCallback

--- src/component/form.js
+++ src/component/form.js
@@ -1,48 +1,45 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import ReturnMovies from './returnMovies';
 
 export const FormComponent = () => {
     const [returnMovies, setReturnMovie] = useState([]);
     const [movieText, setMovie] = useState('');
-    const [queryString, setQuery] = useState('best man');
     const key = 'e25361da';
      //function to update name
      const updateMovie = (e) => {
         setMovie(e.target.value); 
     }
 
-    const updateQuery = e => {
-        e.preventDefault();
-        setQuery(movieText);
-    }
+    const searchMovie = useCallback(
+        () => {
+            fetch(`http://www.omdbapi.com/?apikey=${key}&s=${movieText}`)
+            .then(res => res.json())
+            .then(resMovie => {
+                setReturnMovie(resMovie);
+                setMovie('');
+                console.log(returnMovies);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        },
+        [movieText, returnMovies]
+    )
     
     useEffect(() => {
-        fetch(`http://www.omdbapi.com/?apikey=${key}&s=${queryString}`)
-        .then(res => res.json())
-        .then(resMovie => {
-            setReturnMovie(resMovie);
-            setMovie('');
-            console.log(returnMovies);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, [queryString, returnMovies])
+        searchMovie();
+    }, [searchMovie]);
 
     return(
        <div>
-            <form id="movieForm" onSubmit={updateQuery}>
+            <form id="movieForm" onSubmit={searchMovie}>
                 <div className="form-group">
                     <label>Enter a search word</label>
                     <input type="text" className="form-control" value={movieText} onChange = {updateMovie} name="movieText" placeholder="Enter a movie keyword" />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
-            {
-                returnMovies.map(returnMovie => (
-                    <ReturnMovies />
-                ))
-            }
+            
        </div>
     )
 }
